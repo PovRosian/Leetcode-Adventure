d55f0286379eccc6f42edb0b40c889de997a272b
PovRosian <147337278+PovRosian@users.noreply.github.com>
2023-10-17 14:00:37 +0100

    Time: 19 ms (18.78%), Space: 13.4 MB (10.8%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,16 +1,8 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        if len(word1) == 0 and len(word2) == 0:
-            return ""
-        elif len(word1) == 0 and len(word2) != 0:
+        if not word1:
             return word2
-        elif len(word2) == 0 and len(word1) != 0:
+        if not word2:
             return word1
-        else:
-            return word1[0] + word2[0] + self.mergeAlternately(word1[1:], word2[1:])
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
         
+        return word1[0] + word2[0] + self.mergeAlternately(word1[1:], word2[1:])
